638ca0bb7043a1bfb264492f7984b4f94f7419dc
Alyssa Hursh <akhursh@gmail.com>
2016-11-13 18:16:46 -0800

    Added text replacement for Steve Bannon

--- 1.0_0/content_script.js
+++ 1.0_0/content_script.js
@@ -38,6 +38,8 @@ function handleText(textNode)
 	v = v.replace(/(Donald J\. Trump)/gi, "****** * *****"); // Replace "****** * *****" with any text of your choice
 	v = v.replace(/(Trump)/gi, "*****"); // Replace the "*****" with any text of your choice
 
+	v = v.replace(/(Steve Bannon)/gi, "noted white supremacist Steve Bannon");
+
 
 
 	textNode.nodeValue = v;
